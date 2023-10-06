0483deccfece22556e9d78d1dfcceaa1551ab998
namecoco256 <harinezumi-o_n-o_k-2016@outlook.jp>
2023-10-06 13:43:47 +0900

    入力表示ができたよ! やったねt

--- src/App.vue
+++ src/App.vue
@@ -54,6 +54,7 @@ for (var i = 0; i < battan_position.length; i++) {
   battan_position[i] = [{ 'x': 0, 'y': 0 }, { 'x': 0, 'y': 0 }, { 'x': 0, 'y': 0 }, { 'x': 0, 'y': 0 }, { 'x': 0, 'y': 0 }];
 }
 
+
 //バッタン色を指定。これを使ってバッタンがそこにいるかどうか判定する
 const minColor = { r:60, g:60, b:60 }
 const maxColor = { r:90, g:90, b:90 }
@@ -62,7 +63,7 @@ const timerMinColor = [{ r: 80, g: 100, b: 80 }, { r: 1, g: 33, b: 84 }]
 const timerMaxColor = [{ r: 155, g: 150, b: 118 }, { r: 20, g: 43, b: 58 }]
 
 //ミニゲーム進行中か判定をするかしないか
-const isJudgeGaming = ref(true)
+const isJudgeGaming = ref(false)
 
 let isGaming = false
 ///// 変数・配列の定義ここまで /////
--- src/components/InputDisplay.vue
+++ src/components/InputDisplay.vue
@@ -27,14 +27,56 @@ let canvasCtx
 onMounted(()=>{
   canvas = document.getElementsByClassName('canvas')[1];   //mainキャンバスの要素を取得
   canvasCtx = canvas.getContext('2d');                //2D描画コンテキストを取得
+
+  bs = canvas.height / 25;
+  makeMap()
+  paint()
 })
 
-//描画
+function makeMap(){
+	for(var a=0; a<block.length; a++){
+		for(var b=0; b<block[a].length; b++){
+			// その他のマス(0)
+			block[a][b] = 0;
+			// 角のマス(-1)
+			if(a + b <= 7)block[a][b] = -1;	//左上の角
+			else if(a <= 7 && b > 14 + a)block[a][b] = -1;	//右上の角
+			else if(b <= 9 && a > 14 + b)block[a][b] = -1;	//左下の角
+			else if(a + b >= 37)block[a][b] = -1;	//右下の角
+		}
+	}
+	// バッタンの生息する座標を登録
+	var y_sum = 1;
+	for(var y=0; y<11; y++){
+		for(var x=0; x<5; x++){
+			//はじっこを除外
+			if(x == 0 && (y == 0 || y == 1 || y == 9 || y == 10))continue;
+			if(x == 3 && (y == 0 || y == 10))continue;
+			if(x == 4 && (y % 2 == 0 || y == 1 || y == 9))continue;
+
+			for(var i=0; i<3; i++){
+				if(y%2==0){	//横向き
+					block[y_sum][x*4+4+i] = 100*y+x;
+				}else{		//縦向き
+					block[y_sum+i][x*4+3] = 100*y+x;
+				}
+			}
+		}
+		if(y%2==0)y_sum += 1;
+		else y_sum += 3;
+	}
+}
+
 watch(() => props.battanInput, () => {
+  paint()
+},{deep:true})
+
+//描画
+function paint(){
   console.log('i am a display')
   canvasCtx.strokeStyle = "gray"
   canvasCtx.fillStyle = "white";
-  canvasCtx.fillRect(0, 0, canvas.width * 3, canvas.height * 3);
+  canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
   // 入力エリア
   for (var y = 0; y < 23; y++) {
     for (var x = 0; x < 23; x++) {
@@ -59,18 +101,6 @@ watch(() => props.battanInput, () => {
         } else if (props.battanInput[by][bx] == 2) {
           canvasCtx.fillStyle = "lightgreen";
 
-        } else if (props.battanPatternMatchNum != -1) {
-          // 差分バッタン
-          if (props.battanMerge[by][bx] == 1) {
-            canvasCtx.fillStyle = "orange";
-            // 確定バッタン
-          } else if (props.battanMerge[by][bx] == 0) {
-            if (props.battan_pattern[props.battanPatternMatchNum][by][bx] == 1) {
-              canvasCtx.fillStyle = "blue";
-            } else {
-              canvasCtx.fillStyle = "lightgreen";
-            }
-          }
         }
       } else if (block[y][x] == -1) {
         canvasCtx.fillStyle = "white";
@@ -89,7 +119,7 @@ watch(() => props.battanInput, () => {
 
     }
   }
-},{deep:true})
+}
 </script>
 
 <template>
